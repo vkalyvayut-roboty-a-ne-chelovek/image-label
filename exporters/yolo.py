7067b42d886c67fa08e3f86116a621bebd63337b
vkalyvayut-roboty-a-ne-chelovek <148657431+vkalyvayut-roboty-a-ne-chelovek@users.noreply.github.com>
2024-06-26 21:32:21 +0300

    экспорт в формат yolo

--- exporters/yolo.py
+++ exporters/yolo.py
@@ -37,15 +37,99 @@ class YoloExporter:
         label2.grid(column=0, row=1, sticky='nesw')
         entry2.grid(column=0, row=0, sticky='nesw')
 
-        ok_bnt = tkinter.Button(frame, text='Export to YOLO format', command=lambda: self.ask_for_path(
-            validation_percent=validation_percent.get(), test_percent=test_percent.get()))
-        ok_bnt.grid(column=0, row=2)
+        frame3 = tkinter.Frame(frame)
+        frame3.rowconfigure(0, weight=1)
+        frame3.rowconfigure(1, weight=1)
+        frame3.columnconfigure(0, weight=1)
+        frame3.columnconfigure(1, weight=1)
 
-    def ask_for_path(self, validation_percent, test_percent):
+        frame3.grid(column=0, row=2, sticky='nesw')
+
+        export_rect_vals = tkinter.IntVar(value=0)
+        rb1 = ttk.Radiobutton(frame3, variable=export_rect_vals, value=1, text='Export only rectangulars')
+        rb2 = ttk.Radiobutton(frame3, variable=export_rect_vals, value=2, text='Export with polygons (covert polygons to rectangles)')
+
+        rb1.grid(column=0, row=0, sticky='nesw')
+        rb2.grid(column=0, row=1, sticky='nesw')
+
+        ok_bnt = tkinter.Button(frame, text='Export to YOLO format', command=lambda: self.ask_for_path(
+            validation_percent=validation_percent.get(), test_percent=test_percent.get(), export_rect_vals=export_rect_vals.get()))
+        ok_bnt.grid(column=0, row=3)
+
+    def ask_for_path(self, validation_percent, test_percent, export_rect_vals):
+        if export_rect_vals == 0:
+            return
         path = filedialog.askdirectory()
+        self.root.destroy()
+
         if path:
-            self.export({'validation_percent': validation_percent, 'test_percent': test_percent}, path=path)
-            self.root.destroy()
+            options = {'validation_percent': validation_percent,
+                       'test_percent': test_percent,
+                       'export_rect_vals': export_rect_vals}
+            self.export(options, path=path)
 
     def export(self, options, path):
-        pass
+        raw = self.bus.statechart.project.get_files()
+        categories = []
+        for filename, filedata in raw:
+            for figure in filedata['figures']:
+                categories.append(figure['category'].strip())
+
+        categories = list(set(categories))
+
+        data = {}
+        for filename, filedata in raw:
+            data[filename] = []
+            for figure in filedata['figures']:
+                if options['export_rect_vals'] == 1:
+                    if figure['type'] == 'rect':
+                        data[filename].append([
+                            categories.index(figure['category'].strip()),
+                            *self._convert_rect_data_to_yolo_export_data(
+                                figure['points'][0][0],
+                                figure['points'][0][1],
+                                figure['points'][1][0],
+                                figure['points'][1][1]
+                            )])
+                elif options['export_rect_vals'] == 2:
+                    if figure['type'] == 'rect':
+                        data[filename].append([
+                            categories.index(figure['category'].strip()),
+                            *self._convert_rect_data_to_yolo_export_data(
+                                figure['points'][0][0],
+                                figure['points'][0][1],
+                                figure['points'][1][0],
+                                figure['points'][1][1]
+                            )])
+                    elif figure['type'] == 'poly':
+                        data[filename].append([
+                            categories.index(figure['category'].strip()),
+                            *self._convert_poly_data_to_yolo_export_data(figure['points'])])
+
+
+    @staticmethod
+    def _convert_rect_data_to_yolo_export_data(x1, y1, x2, y2):
+        min_x, max_x, min_y, max_y = min(x1, x2), max(x1, x2), min(y1, y2), max(y1, y2)
+        w, h = max_x - min_x, max_y - min_y
+
+        center_x = min_x + w / 2.0
+        center_y = min_y + h / 2.0
+
+        return center_x, center_y, w, h
+
+    @staticmethod
+    def _convert_poly_data_to_yolo_export_data(points):
+        xs = [point[0] for point in points]
+        ys = [point[1] for point in points]
+        min_x, max_x, min_y, max_y = min(xs), max(xs), min(ys), max(ys)
+
+        w, h = max_x - min_x, max_y - min_y
+
+        center_x = min_x + w / 2.0
+        center_y = min_y + h / 2.0
+
+        return center_x, center_y, w, h
+
+
+
+
